7a74c8f4ef3728446ae6ea78e5af14759a43326a
Elizabete <elizabete958@gmail.com>
2024-04-12 15:21:31 -0300

    Alter nome do arquivo

---
+++ burger-queen/src/app/Layout/Components/registrar-pedido/registrar-pedido.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrarPedidoComponent } from './registrar-pedido.component';
+
+describe('RegistrarPedidoComponent', () => {
+  let component: RegistrarPedidoComponent;
+  let fixture: ComponentFixture<RegistrarPedidoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegistrarPedidoComponent]
+    });
+    fixture = TestBed.createComponent(RegistrarPedidoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ burger-queen/src/app/Layout/Components/registrar-pedido/registrar-pedido.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-pedido',
+  templateUrl: './registrar-pedido.component.html',
+  styleUrls: ['./registrar-pedido.component.css']
+})
+export class RegistrarPedidoComponent {
+
+}
