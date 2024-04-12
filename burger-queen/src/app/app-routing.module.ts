4b138ca1372909a0b2ee3c7ead3f83d722356ad8
Elizabete <elizabete958@gmail.com>
2024-04-12 14:32:21 -0300

    add path de registrar-mesa

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Layout/Pages/home/home.component';
 import { LoginComponent } from './Layout/Components/login/login.component';
 import { CadastroComponent } from './Layout/Components/cadastro/cadastro.component';
+import { RegistrarMesaComponent } from './Layout/Components/registrar-mesa/registrar-mesa.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
+  { path: 'registrar-mesa', component: RegistrarMesaComponent }
+
 ];
 
 @NgModule({
