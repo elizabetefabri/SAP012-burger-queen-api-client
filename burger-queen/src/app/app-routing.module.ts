286fd228c9d6df7ae8b038e40b9fe29c6813c951
Elizabete <elizabete958@gmail.com>
2024-04-12 10:13:16 -0300

    Redirecionamento das paginas

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Layout/Pages/home/home.component';
+import { LoginComponent } from './Layout/Components/login/login.component';
+import { CadastroComponent } from './Layout/Components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
