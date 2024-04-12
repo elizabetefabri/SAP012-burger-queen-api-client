1cbdb5299e5b6b87861181913ff7d09dcb469dbc
Elizabete <elizabete958@gmail.com>
2024-04-12 10:44:02 -0300

    estrutura inicial da conexao

--- burger-queen/src/app/Layout/Components/login/login.component.ts
+++ burger-queen/src/app/Layout/Components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from 'src/app/Shared/Services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  email: string = '';
+  password: string = '';
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onLogin(): void {
+    this.auth.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('====================================');
+        console.log("Login realizado com Sucesso", response);
+        console.log('====================================');
+      },
+      error: (error) => {
+        console.error("Seu Login Falhou 💣", error);
+      }
+    })
+  }
+
+
 
 }
