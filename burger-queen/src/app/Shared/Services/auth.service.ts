648713f26ab97b95ce471db1b136d10f3463b396
Elizabete <elizabete958@gmail.com>
2024-04-12 10:40:45 -0300

    add config inicial do login com o formulario

--- burger-queen/src/app/Shared/Services/auth.service.ts
+++ burger-queen/src/app/Shared/Services/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  readonly apiUrl: string = environment.API_URL;
 
-  constructor() { }
+  constructor(
+    readonly http: HttpClient,
+   ) { }
+
+   login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+   // Método para verificar se o usuário está logado
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  // Método para logout
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+// o serviço aqui utilizado ira ser realizado somente para logar
 }
