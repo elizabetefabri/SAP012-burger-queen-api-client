a749ffa6d94fc64a920a55d082e09e4ebfcaa227
Elizabete <elizabete958@gmail.com>
2024-04-17 20:04:39 -0300

    Testes do guards de autenticação de login

--- burger-queen/src/app/guards/auth.guard.spec.ts
+++ burger-queen/src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthGuard } from './auth.guard';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let router: Router;
+  let routeMock: Partial<ActivatedRouteSnapshot>;
+  let stateMock: Partial<RouterStateSnapshot>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [AuthGuard]
+    });
+
+    guard = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router);
+    routeMock = {};
+    stateMock = { url: '/some-url' };
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should return true for canActivate if token exists', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('fake-token');
+    expect(guard.canActivate(routeMock as ActivatedRouteSnapshot, stateMock as RouterStateSnapshot)).toBeTruthy();
+  });
+
+  it('should redirect to login if no token exists', () => {
+    spyOn(localStorage, 'getItem').and.returnValue(null);
+    spyOn(router, 'createUrlTree').and.returnValue(new UrlTree());
+    expect(guard.canActivate(routeMock as ActivatedRouteSnapshot, stateMock as RouterStateSnapshot)).toEqual(router.createUrlTree(['/login']));
   });
 });
