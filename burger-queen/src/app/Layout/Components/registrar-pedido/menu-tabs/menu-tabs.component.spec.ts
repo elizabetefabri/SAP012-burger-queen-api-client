89fe8ea16b21cac1c8aec47cbbb5aec62be3da11
Elizabete <elizabete958@gmail.com>
2024-04-17 21:58:27 -0300

    Organização dos testes

--- burger-queen/src/app/Layout/Components/registrar-pedido/menu-tabs/menu-tabs.component.spec.ts
+++ burger-queen/src/app/Layout/Components/registrar-pedido/menu-tabs/menu-tabs.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuTabsComponent } from './menu-tabs.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CardsPedidoComponent } from '../cards-pedido/cards-pedido.component';
+import { FinalizarPedidoComponent } from '../finalizar-pedido/finalizar-pedido.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MenuTabsComponent', () => {
   let component: MenuTabsComponent;
@@ -8,7 +12,15 @@ describe('MenuTabsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MenuTabsComponent]
+      declarations: [
+        MenuTabsComponent,
+        CardsPedidoComponent,
+        FinalizarPedidoComponent
+      ],
+      imports: [
+        MatTabsModule,
+        NoopAnimationsModule
+      ]
     });
     fixture = TestBed.createComponent(MenuTabsComponent);
     component = fixture.componentInstance;
--- burger-queen/src/app/Layout/Components/registrar-pedido/registrar-pedido.component.ts
+++ burger-queen/src/app/Layout/Components/registrar-pedido/registrar-pedido.component.ts
@@ -1,10 +1,23 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registrar-pedido',
   templateUrl: './registrar-pedido.component.html',
-  styleUrls: ['./registrar-pedido.component.css']
+  styleUrls: ['./registrar-pedido.component.css'],
+  animations: [
+    trigger('translateTab', [
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition('* => void', [
+        animate('500ms ease-in-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
+
 export class RegistrarPedidoComponent {
 
 }
