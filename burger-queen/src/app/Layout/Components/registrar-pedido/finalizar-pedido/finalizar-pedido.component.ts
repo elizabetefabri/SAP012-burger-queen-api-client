469cb544aa547a3c05a28af58583669daa075c24
Elizabete <elizabete958@gmail.com>
2024-04-21 16:48:53 -0300

    Primeira tentativa de listar os produtos, e realizar a busca dos produtos somando o total

--- burger-queen/src/app/Layout/Components/registrar-pedido/finalizar-pedido/finalizar-pedido.component.ts
+++ burger-queen/src/app/Layout/Components/registrar-pedido/finalizar-pedido/finalizar-pedido.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/Shared/Services/auth.service';
+import { ProductsService } from 'src/app/Shared/Services/Products/products.service';
+import { Products } from 'src/Models/Produto';
 
 @Component({
   selector: 'app-finalizar-pedido',
   templateUrl: './finalizar-pedido.component.html',
   styleUrls: ['./finalizar-pedido.component.css']
 })
-export class FinalizarPedidoComponent {
 
+export class FinalizarPedidoComponent implements OnInit{
+  @Input() selectedProducts: { product: Products, quantity: number }[] = [];
+  mesaId: string = '';
+
+  constructor(private authService: AuthService, private productService: ProductsService, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.getMesaIdFromUrl();
+  }
+
+  getMesaIdFromUrl(): void {
+    this.route.queryParams.subscribe(params => {
+      this.mesaId = params["mesaId"] || '';
+    });
+  }
+
+  getTotal(): number {
+    return this.selectedProducts.reduce((total, item) => total + (item.product.price * item.quantity), 0);
+  }
+
+  removeItem(index: number): void {
+    this.selectedProducts.splice(index, 1);
+  }
 }
