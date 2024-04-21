f7d4bc1b1df446394649b6afc2148a0bd743e12d
Elizabete <elizabete958@gmail.com>
2024-04-20 21:50:26 -0300

    Add rotas do id mesa para pagina de registrar pedido

--- burger-queen/src/app/Layout/Components/registrar-mesa/registrar-mesa.component.ts
+++ burger-queen/src/app/Layout/Components/registrar-mesa/registrar-mesa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/Shared/Services/auth.service';
+import { ProductsService } from 'src/app/Shared/Services/Products/products.service';
 
 @Component({
   selector: 'app-registrar-mesa',
@@ -8,10 +9,15 @@ import { AuthService } from 'src/app/Shared/Services/auth.service';
   styleUrls: ['./registrar-mesa.component.css']
 })
 export class RegistrarMesaComponent implements OnInit{
+  mesaId: string = '';
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: ActivatedRoute) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.router.queryParams.subscribe(params => {
+      this.mesaId = params["mesaId"] || '';
+    });
+   }
 
   deslogar(){
     this.auth.logout()
