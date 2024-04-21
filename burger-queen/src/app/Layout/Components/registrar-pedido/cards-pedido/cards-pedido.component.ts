0599b2ece4d6b0bcf13037f7d38b58eabdf807db
Elizabete <elizabete958@gmail.com>
2024-04-21 16:41:46 -0300

    add logica de soma total cards e estilização

--- burger-queen/src/app/Layout/Components/registrar-pedido/cards-pedido/cards-pedido.component.ts
+++ burger-queen/src/app/Layout/Components/registrar-pedido/cards-pedido/cards-pedido.component.ts
@@ -9,17 +9,21 @@ import { Products } from 'src/Models/Produto';
 })
 export class CardsPedidoComponent implements OnInit{
   @Input() type: string = '';
-  products: Products[] = [];
+  @Input() products: Products[] = [];
+  // @Input() beverages: Products[] = [];
+  // @Input() lunch: Products[] = [];
+  // @Input() breakfast: Products[] = [];
+  @Input() selectedProducts: { product: Products, quantity: number }[] = [];
+
+  total: number = 0;
+
+  // selectedProducts: { product: Products, quantity: number }[] = [];
 
   constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
     this.loadProducts();
   }
-
-  // get filteredProducts(): Products[] {
-  //   return this.products.filter(product => product.tipo === this.type);
-  // }
 
   loadProducts(){
     this.productService.listProductsByType(this.type).subscribe({
@@ -31,6 +35,37 @@ export class CardsPedidoComponent implements OnInit{
         console.log('Erro ao carregar os produtos: ', error);
       }
     });
+  }
+
+  addToAnottation(product: Products): void {
+    const index = this.selectedProducts.findIndex(item => item.product === product);
+    if (index !== -1) {
+      this.selectedProducts[index].quantity++;
+    } else {
+      this.selectedProducts.push({ product, quantity: 1 });
+    }
+    this.calculateTotal();
+  }
+
+  removeFromAnottation(product: Products): void {
+    const index = this.selectedProducts.findIndex(item => item.product === product);
+    if (index !== -1) {
+      if (this.selectedProducts[index].quantity > 1) {
+        this.selectedProducts[index].quantity--;
+      } else {
+        this.selectedProducts.splice(index, 1);
+      }
+    }
+    this.calculateTotal();
+  }
+
+  getQuantity(product: Products): number {
+    const item = this.selectedProducts.find(item => item.product === product);
+    return item ? item.quantity : 0;
+  }
+
+  calculateTotal(): void {
+    this.total = this.selectedProducts.reduce((total, item) => total + (item.product.price * item.quantity), 0);
   }
 }
 
