3013989b90f277ebd2c915d06dc5a8b6156bc7a6
Elizabete <elizabete958@gmail.com>
2024-05-11 17:49:51 -0300

    add config inicial para utilizar a estilização do tailwindcss e material angular

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -1,24 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Layout/Components/login/login.component';
-import { HomeComponent } from './Layout/Pages/home/home.component';
 import { CadastroComponent } from './Layout/Components/cadastro/cadastro.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { RegistrarMesaComponent } from './Layout/Components/registrar-mesa/registrar-mesa.component';
-import { CardsMesaComponent } from './Layout/Components/registrar-mesa/cards-mesa/cards-mesa.component';
-import { RegistrarPedidoComponent } from './Layout/Components/registrar-pedido/registrar-pedido.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import { CardsPedidoComponent } from './Layout/Components/registrar-pedido/cards-pedido/cards-pedido.component';
-import { MenuTabsComponent } from './Layout/Components/registrar-pedido/menu-tabs/menu-tabs.component';
-import { FinalizarPedidoComponent } from './Layout/Components/registrar-pedido/finalizar-pedido/finalizar-pedido.component';
-import { TotalPedidoComponent } from './Layout/Components/registrar-pedido/total-pedido/total-pedido.component';
 import { HeaderComponent } from './Layout/Components/header/header.component';
+import { LoginComponent } from './Layout/Components/login/login.component';
+import { CardsMesaComponent } from './Layout/Components/registrar-mesa/cards-mesa/cards-mesa.component';
+import { RegistrarMesaComponent } from './Layout/Components/registrar-mesa/registrar-mesa.component';
+import { CardsPedidoComponent } from './Layout/Components/registrar-pedido/cards-pedido/cards-pedido.component';
+import { FinalizarPedidoComponent } from './Layout/Components/registrar-pedido/finalizar-pedido/finalizar-pedido.component';
+import { MenuTabsComponent } from './Layout/Components/registrar-pedido/menu-tabs/menu-tabs.component';
+import { RegistrarPedidoComponent } from './Layout/Components/registrar-pedido/registrar-pedido.component';
+import { TotalPedidoComponent } from './Layout/Components/registrar-pedido/total-pedido/total-pedido.component';
+import { HomeComponent } from './Layout/Pages/home/home.component';
 
 @NgModule({
   declarations: [
---
+++ burgers-queen/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      "Lato": ['Lato', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        "color-header": "#18181B",
+        "color-background": "#191919 ",
+        "color-background-logado": "#1e1e1e",
+        "color-border-gray": "#333333",
+        "color-font-gray": "#575151",
+        "color-gray-icon": "#919191",
+        "color-main": "#262626",
+        "color-font-white": "#F9FAFB",
+        "color-font-orange": "#C2410C",
+        "color-font-green": "#459975",
+      },
+      maxWidth: {
+        '1440': '1440px',
+      },
+    },
+  },
+  plugins: [],
+}
+
