478a9ab3ee624d6a69957ec9f5559d1b99ce6e51
Elizabete <elizabete958@gmail.com>
2024-05-18 18:57:17 -0300

    refactor para autenticar usuario e enviar o pedido

--- burger-queen/src/app/Layout/Components/menu/modal/modal.component.ts
+++ burger-queen/src/app/Layout/Components/menu/modal/modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Item } from 'src/app/Models/Order';
+import { Item, Order } from 'src/app/Models/Order';
 import { Product } from 'src/app/Models/Produto';
+import { AuthService } from 'src/app/Shared/Services/Authentication/auth.service';
 import { OrderService } from 'src/app/Shared/Services/Orders/order.service';
 import { ProductService } from 'src/app/Shared/Services/Products/product.service';
-
+import { v4 as uuidv4 } from "uuid";
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
@@ -31,6 +32,7 @@ export class ModalComponent implements OnInit {
   items: Item[] = [];
 
   constructor(
+    private auth: AuthService,
     private productService: ProductService,
     private orderService: OrderService,
     private router: Router,
@@ -38,6 +40,7 @@ export class ModalComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.auth.getCurrentUser();
     this.getMesaIdFromUrl();
     this.orderService.items$.subscribe((items) => {
       this.items = items;
@@ -93,10 +96,46 @@ export class ModalComponent implements OnInit {
 
   calculateTotal(): void {
     this.totalPedido = this.items.reduce(
-      (total, item) => total + item.product.price * item.quantity,
-      0
-    );
+      (total, item) => total + (item.product.price * item.quantity), 0);
   }
 
-  enviarPedido(): void {}
+  enviarPedido(): void {
+    const currentUser = this.auth.getCurrentUser();
+    if (!currentUser) {
+      console.error('Usuário não autenticado');
+      this.router.navigate(['/login']); // Redireciona para a página de login
+      return;
+    }
+    const userId = currentUser.id; // Obtém o ID do usuário logado
+    const dateEntry = new Date().toISOString();
+    const clientId = uuidv4(); // Gera um ID único para o cliente
+
+    const order: Partial<Order> = {
+      userId: Number(userId),
+      clientId: clientId, // Adiciona o clientId ao pedido
+      client: this.nomeCadastrado,
+      items: this.items.map(item => ({
+        product: item.product,
+        quantity: item.quantity
+      })),
+      status: "pending",
+      dateEntry: dateEntry,
+      mesaId: this.mesaId // Adiciona o número da mesa
+    };
+
+    this.orderService.postOrder(order as Order).subscribe(response => {
+      console.log('Order created successfully', response);
+      this.clearOrder();
+      this.closeModal(); // Fecha o modal automaticamente
+      this.router.navigate(['/chef-orders']); // Redireciona para a página de pedidos do chefe
+    }, error => {
+      console.error('Error creating order', error);
+    });
+  }
+
+  clearOrder(): void {
+    this.orderService.clearItems();
+    this.nomeCadastrado = '';
+    this.totalPedido = 0;
+  }
 }
